582e48f920942c6c941e2960dfaff81bae4811e4
Jérémie Fouché <jeremie.fouche@gmail.com>
2014-02-08 11:48:47 +0100

    [lua script] simplified the "codelite" global variable lua implementation

--- LuaScript/lua_codelite.cpp
+++ LuaScript/lua_codelite.cpp
@@ -1,8 +1,6 @@
 #include "lua_utils.hpp"
 #include "imanager.h"
 
-static const char* CLASSNAME = "Codelite";
-
 static int Trace(lua_State* L)
 {
 	const char* message = lua_tostring(L, 1);
@@ -17,12 +15,8 @@ static const luaL_Reg METHODS[] = {
 
 void lua_open_Codelite(lua_State* L, IManager* manager, const char* name)
 {
-	luaL_newmetatable(L, CLASSNAME);
 	lua_createtable(L, 0, 0);
-	lua_pushvalue(L, 1); // keep a copy of table, to set it global
 	luaL_setfuncs(L, METHODS, 0);
-	lua_setfield(L, -3, "__index");
-	lua_pop(L, 1);
 
 	// add the 'manager' field
 	lua_pushstring(L, "manager");
