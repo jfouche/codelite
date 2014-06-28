36765e72cb72bf2b56a2529bb3ca370c1e8cc00a
Jérémie Fouché <jeremie.fouche@gmail.com>
2014-06-28 13:13:04 +0200

    [luascript] updated tests

--- LuaScript/tests/tests.lua
+++ LuaScript/tests/tests.lua
@@ -1,14 +1,30 @@
-codelite.Trace("Launching tests")
+function assertEquals(expected, value)
+	message = "Assertion failed. Expected [" .. expected .. "], got [" .. value .. "]"
+	assert(expected == value, message)
+end
 
+-- Starts tests
+codelite.Trace("Launching tests")
 editor = codelite.manager:NewEditor()
 
-assert(editor:GetEditorText() == "")
-editor:SetEditorText("text")
-assert(editor:GetEditorText() == "text")
+assertEquals("", editor:GetEditorText())
+editor:SetEditorText("world")
+assertEquals("world", editor:GetEditorText())
+editor:InsertText(0, "hello ")
+assertEquals("hello world", editor:GetEditorText())
+editor:AppendText("!")
+assertEquals("hello world!", editor:GetEditorText())
 
-text = editor:GetSelection()
-assert(editor:GetSelection() == "")
+-- caret
+assertEquals(0, editor:GetCurrentPosition())
+
+-- Selection
+assertEquals("", editor:GetSelection())
 editor:SelectText(1, 2)
-assert(editor:GetSelection() == "ex")
-editor:SelectText(1, 18)
-assert(editor:GetSelection() == "ext")
+assertEquals("el", editor:GetSelection())
+editor:SelectText(6, 18)
+assertEquals("world!", editor:GetSelection())
+editor:ReplaceSelection("WORLD!")
+assertEquals("hello WORLD!", editor:GetEditorText())
+editor:SelectText(5, 0)
+assertEquals("", editor:GetSelection())
